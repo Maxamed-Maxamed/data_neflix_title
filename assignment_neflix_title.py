4b58e4d44e3bd38f2dc7e5555e748df6699e7b8f
Maxamed Maxamed <20088349@mail.wit.ie>
2024-06-18 18:20:33 +0100

    Added import statements for various Python libraries.

---
+++ assignment_neflix_title.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns 
+from scipy.stats import ttest_ind 
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+import statsmodels.formula.api as smf
+import statsmodels.stats.multicomp as multi
+
+
